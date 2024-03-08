9121fce40e5e91d0dea3b9034d6dff4778870726
TechnoGoku <bulatikilya@gmail.com>
2024-03-08 19:49:17 +0200

    update_2

--- hw_2.py
+++ hw_2.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    unique_random_numbers = []
+
+
+
+
+min_number = int(input("Введіть вашу мінімальну цифру не менше 1: "))
+max_number = int(input("Введіть вашу максимальну цифру не більше 1000: "))
+quantity_number = int(input("Введіть кількість чисел, які потрібно обрати між мінимальним та максимальним значенням: "))
+get_numbers_ticket(min_number, max_number, quantity_number)
+print(min_number)
+print(max_number)
